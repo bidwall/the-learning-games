41ae861a51da07e73dad85c32f11803c2d88a251
Palvinder Bidwall <pal@bidwall.co.uk>
2017-06-03 11:02:33 +0100

    Restructured src folder & added numbers game component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+
+import NumbersGame from './NumbersGame/NumbersGame';
 
 class App extends Component {
   render() {
@@ -8,11 +10,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the Learning Games</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          It all starts from <code>src/App.js</code>, have a look and follow the trail...
         </p>
+        <div>
+          <NumbersGame></NumbersGame>
+        </div>
       </div>
     );
   }
---
+++ src/components/NumbersGame/NumbersGame.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function NumbersGame() {
+    return <h1>yeeh, it's the numbers game!!!</h1>
+}
+
+export default NumbersGame;
