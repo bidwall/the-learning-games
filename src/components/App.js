959dde3d90ae87451e5a5a786bd8e522a22cfa47
Palvinder Bidwall <pal@bidwall.co.uk>
2017-06-03 11:22:58 +0100

    Tided App component

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to the Learning Games</h2>
+          <h2>The Learning Games</h2>                  
         </div>
-        <p className="App-intro">
-          It all starts from <code>src/App.js</code>, have a look and follow the trail...
-        </p>
         <NumbersGame></NumbersGame>
       </div>
     );
