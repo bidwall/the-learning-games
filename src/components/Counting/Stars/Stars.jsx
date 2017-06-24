bd9a1684e7ffc44f47722aecde7c608c967161f5
Palvinder Bidwall <pal@bidwall.co.uk>
2017-06-24 20:21:31 +0100

    Remove css file and used inline styling for star component

--- src/components/Counting/Stars/Stars.jsx
+++ src/components/Counting/Stars/Stars.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Col } from 'reactstrap';
 import FontAwesome from 'react-fontawesome';
 
+const starStyle = {
+    margin: '0.5em',
+    'font-size': '28px'
+};
+
 function Stars(props) {
     let stars = [...Array(props.numberOfStars).keys()]
-                    .map(x => <FontAwesome name="star" key={x}></FontAwesome>);
+                    .map(x => <FontAwesome name="star" key={x} style={starStyle}></FontAwesome>);
 
     return (
         <Col>
